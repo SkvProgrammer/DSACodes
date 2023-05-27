5ce16e0b259ba8d6057d09df26e8de7cd9f40823
skvprogrammer <satyamvarman2004@gmail.com>
2023-05-27 14:49:22 +0530

    two important questions of bit manipulation in comments

---
+++ power2programcheck.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    //write a program to check if the given number is a power of 2
+    //write a program to generate all the possible subsets of a set {a,b,c}
+   
+
+ 
+}
